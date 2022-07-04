a19551ff2ba716bcc5e2b6ec7d86c9a3d367cc77
Gian Marco Cinalli <gm.cinalli@gmail.com>
2022-07-04 20:29:43 +0200

    Adds tree view to exa's aliases.

--- sheldon/plugins/aliases/aliases.zsh
+++ sheldon/plugins/aliases/aliases.zsh
@@ -3,3 +3,4 @@
 alias ls="exa --icons --group --group-directories-first"
 alias ll="exa --icons --group --group-directories-first --long"
 alias lla="exa --icons --group --group-directories-first --long --all"
+alias llt="exa --icons --group --group-directories-first --long --all --tree"
